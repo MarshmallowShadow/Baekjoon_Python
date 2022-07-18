748404bf5bf2d4c0c3d178d89af41b679e959502
MarshmallowShadow <choiboseung@naver.com>
2022-07-18 11:12:18 +0900

    test

--- bj010872.py
+++ bj010872.py
@@ -1,3 +1,11 @@
-C = input()
+def factorial(num):
+    if num == 0:
+        return 1
+    recurse = num * factorial(num-1)
+    return recurse
 
-print(C)
+
+N = int(input())
+print(factorial(N))
+
+
